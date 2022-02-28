9c86e1d156d9cb5fed56ea09294f4437dd281314
marvil-exe <marvil.tech@gmail.com>
2022-02-28 12:31:53 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -360,4 +360,13 @@
 		$(this).blur();
 	});
 
+    function copy(that){
+        var inp =document.createElement('input');
+        document.body.appendChild(inp)
+        inp.value =that.textContent
+        inp.select();
+        document.execCommand('copy',false);
+        inp.remove();
+        }
+
 })(jQuery);
